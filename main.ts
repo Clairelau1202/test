a901b9f64f9cb351e1396c4999353e0baf80520e
Clairelau1202 <102672955+Clairelau1202@users.noreply.github.com>
2022-03-30 21:54:19 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
-input.onGesture(Gesture.Shake, function () {
-	
-})
+let direction = 0
 basic.forever(function () {
-	
+    direction = input.compassHeading()
+    if (direction <= 45 || direction >= 315) {
+        basic.showArrow(ArrowNames.North)
+    } else if (direction > 45 && direction < 135) {
+        basic.showArrow(ArrowNames.West)
+    } else if (direction >= 135 && direction < 225) {
+        basic.showArrow(ArrowNames.East)
+    } else if (direction >= 255 && direction < 315) {
+        basic.showArrow(ArrowNames.South)
+    }
 })
